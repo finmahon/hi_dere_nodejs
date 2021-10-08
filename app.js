d2fc7b06dae4d1156b67348f715dd048cc102f8e
fmahon <fmahon@redhat.com>
2021-10-08 17:04:23 +0100

    add logging

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const port = 3000
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('hi dere')
+app.get('/*', (req, res) => {
+  console.log(new Date() + "  " + req.url)
+  res.send('hey dere')
 })
 
 app.listen(port, () => {
