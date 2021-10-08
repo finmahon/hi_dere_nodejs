027df4ccda0736c4a2cb57167037016e4af70665
fmahon <fmahon@redhat.com>
2021-10-08 17:56:15 +0100

    more endpoints

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const port = 3000
 
 app.use(cors())
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   console.log(new Date() + "  " + req.url)
   res.send('hey dere')
+})
+
+app.get('/wut', (req, res) => {
+  console.log(new Date() + " wut  " + req.url)
+  res.send('hey dere 2')
+})
+
+app.get('/*', (req, res) => {
+  console.log(new Date() + " default  " + req.url)
+  res.send('hey dere default')
 })
 
 app.listen(port, () => {
