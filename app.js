de0e9dd646ca7b2a11eb0c513eaa656a23f5974e
fmahon <fmahon@redhat.com>
2021-10-10 11:52:10 +0100

    add PORT Env Variable

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 var cors = require('cors')
+
+let path = require('path')
 const health = require('@cloudnative/health-connect');
 let healthcheck = new health.HealthChecker();
 const app = express()
-const port = 3000
+const port =  process.env.OPENSHIFT_NODEJS_PORT || 3000
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/live', health.LivenessEndpoint(healthcheck))
+app.use('/ready', health.ReadinessEndpoint(healthcheck))
+app.use('/health', health.HealthEndpoint(healthcheck))
 
 app.use('/live', health.LivenessEndpoint(healthcheck))
 app.use('/ready', health.ReadinessEndpoint(healthcheck))
@@ -25,6 +32,8 @@ app.get('/*', (req, res) => {
   console.log(new Date() + " default  " + req.url)
   res.send('hey dere default')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
