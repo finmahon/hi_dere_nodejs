f0545c2d16e02601b53dcb824da3c40a58854b81
fmahon <fmahon@redhat.com>
2021-10-10 11:52:10 +0100

    add health probes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 var cors = require('cors')
+const health = require('@cloudnative/health-connect');
+let healthcheck = new health.HealthChecker();
 const app = express()
 const port = 3000
 
 app.use(cors())
+
+app.use('/live', health.LivenessEndpoint(healthcheck))
+app.use('/ready', health.ReadinessEndpoint(healthcheck))
+app.use('/health', health.HealthEndpoint(healthcheck))
 
 app.get('/', (req, res) => {
   console.log(new Date() + "  " + req.url)
