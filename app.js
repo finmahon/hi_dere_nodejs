8be7da893c9d8af3accbd4e8cdee6d5f9ed2da01
fmahon <fmahon@redhat.com>
2021-10-14 22:07:17 +0100

    add additional health listener

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 const express = require('express')
-var cors = require('cors')
-
-let path = require('path')
+const cors = require('cors')
 const health = require('@cloudnative/health-connect');
 let healthcheck = new health.HealthChecker();
 const app = express()
-const port =  process.env.OPENSHIFT_NODEJS_PORT || 3000
+const appHealth = express()
+let port =  process.env.OPENSHIFT_NODEJS_PORT || 3000
+const portHealth =  process.env.OPENSHIFT_NODEJS_PORT_HEALTH || 8080
+
+if (port == portHealth) {
+  port = parseInt(port,10)+1
+}
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+appHealth.use(cors())
 
-app.use('/live', health.LivenessEndpoint(healthcheck))
-app.use('/ready', health.ReadinessEndpoint(healthcheck))
-app.use('/health', health.HealthEndpoint(healthcheck))
-
-app.use('/live', health.LivenessEndpoint(healthcheck))
-app.use('/ready', health.ReadinessEndpoint(healthcheck))
-app.use('/health', health.HealthEndpoint(healthcheck))
-
-app.get('/', (req, res) => {
-  console.log(new Date() + "  " + req.url)
-  res.send('hey dere')
+appHealth.use('/live', health.LivenessEndpoint(healthcheck))
+appHealth.use('/ready', health.ReadinessEndpoint(healthcheck))
+appHealth.use('/health', health.HealthEndpoint(healthcheck))
+appHealth.get('/', (req, res) => {
+  console.log(new Date() + " app health root " + req.url)
+  res.send('hello from health root')
 })
 
-app.get('/wut', (req, res) => {
-  console.log(new Date() + " wut  " + req.url)
-  res.send('hey dere 2')
+appHealth.get('/*', (req, res) => {
+  console.log(new Date() + " default endpoint " + req.url)
+  res.send('hello from default endpoint')
+})
+
+
+app.get('/', (req, res) => {
+  console.log(new Date() + " app root " + req.url)
+  res.send('hello from app root')
+})
+
+app.get('/test1', (req, res) => {
+  console.log(new Date() + " test1 endpoint  " + req.url)
+  res.send('hello from test1')
+})
+
+app.get('/test2', (req, res) => {
+  console.log(new Date() + " test2 endpoint  " + req.url)
+  res.send('hello from test2')
 })
 
 app.get('/*', (req, res) => {
-  console.log(new Date() + " default  " + req.url)
-  res.send('hey dere default')
+  console.log(new Date() + " default endpoint " + req.url)
+  res.send('hello from default endpoint')
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+appHealth.listen(portHealth, () => {
+  console.log(`Example app health listening at http://localhost:${portHealth}`)
 })
